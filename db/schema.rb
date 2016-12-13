3abfe654a7a11b3f5e908f76d7594d996dcbb21a
Duyen Ho <duyenho@gmail.com>
2016-12-13 22:40:19 +1100

    add story model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213023741) do
+ActiveRecord::Schema.define(version: 20161213113850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stories", force: :cascade do |t|
+    t.text     "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text     "name"
