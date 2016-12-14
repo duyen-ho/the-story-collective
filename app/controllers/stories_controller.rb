a317d52289956afd8f16815369570dd66aaf3d8a
Duyen Ho <duyenho@gmail.com>
2016-12-14 12:22:42 +1100

    add stories show, edit and error pages

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -21,6 +21,25 @@ class StoriesController < ApplicationController
   end
 
   def show
+    @story = Story.find_by(id: params[:id])
+    @parts = Part.where(story_id: params[:id]).order(:created_at)
+
+    if @story
+      render :show
+    else
+      @error = 'Story not found'
+      render :error
+    end
+
+  end
+
+  def edit
+    @parts = Part.where(story_id: params[:id]).order(:created_at)
+
+    # TODO: test
+    # if @parts.length == 10
+    #   render :show
+    # end
 
   end
 
