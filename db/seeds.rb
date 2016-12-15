1624b1fa54d99eb6b258c2ade72749e2e4235160
Duyen Ho <duyenho@gmail.com>
2016-12-16 10:00:21 +1100

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,8 @@ usernames = [
   'banoffee',
   'oscar5245',
   'rita_mamasita',
-  'gaitskill'
+  'gaitskill',
+  'blahblah'
 ]
 
 emails = [
@@ -32,10 +33,12 @@ emails = [
   'banoffee@ga.com',
   'oscar5245@ga.com',
   'rita_mamasita@ga.com',
-  'gaitskill@ga.com'
+  'gaitskill@ga.com',
+  'blahblah@ga.com'
 ]
 
 passwords = [
+  'pw2016',
   'pw2016',
   'pw2016',
   'pw2016',
@@ -62,11 +65,11 @@ descriptions = [
 ]
 
 story_titles = [
+  'Fleeting Neuroscience',
   '',
+  'Officia Deserunt Mollitia',
   '',
-  '',
-  '',
-  '',
+  'Walk Through',
   '',
   '',
   '',
@@ -74,40 +77,146 @@ story_titles = [
   '',
 ]
 
-# parts = [
-#   '',
-#   '',
-#   '',
-#   '',
-#   '',
-#   '',
-#   '',
-#   '',
-#   '',
-#   '',
-#   '',
-# ]
+story_1_parts = [
+  'I can’t I can’t I can’t I can’t I can’t I can’t',
+  'I can’t I can’t I can’t I can’t I can’t I can’t',
+  'I can’t I can’t I can’t I can’t I can’t I can’t',
+  'I can’t I can’t I can’t I can’t I can’t I can’t',
+  'I can’t I can’t I can’t I can’t I can’t I can’t',
+  'I can’t I can’t I can’t I can’t I can’t I can’t',
+  'I can’t I can’t',
+  'You can.',
+  'It’s called',
+  'Psycholinguistics.',
+  ''
+]
+
+story_2_parts = [
+  'Rump andouille swine tri-tip tail brisket ball tip sirloin prosciutto frankfurter doner t-bone shankle.',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  ''
+]
+
+story_3_parts = [
+  'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa quae.',
+  'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?',
+  'At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.',
+  'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit.',
+  'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae.',
+  'Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut.',
+  'Autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat.',
+  'Molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga.',
+  'Quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam.',
+  'Aliquam quaerat voluptatem voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis.',
+  ''
+]
+
+story_4_parts = [
+  'Filet mignon burgdoggen shoulder chicken. Jerky bresaola kielbasa flank frankfurter.',
+  'Meatloaf shoulder filet mignon, tenderloin brisket landjaeger shankle. Short ribs rump salami, sirloin short loin tail corned beef.',
+  'Bresaola ribeye landjaeger ham hock pastrami pork chop tongue pork loin meatball jowl burgdoggen.',
+  'Doner corned beef pig drumstick short ribs tenderloin turducken, spare ribs shankle prosciutto biltong leberkas.',
+  '',
+  '',
+  '',
+  '',
+  '',
+  ''
+]
+
+story_5_parts = [
+  'He groped for a while in the darkness and when the lights came back on he found himself on the third floor. This particular skill,',
+  'apparently unrelated to any aspiration of his, rather disturbed him. He felt within him a need for expansion, a growing desire to fulfil',
+  'and surpass himself, a stab of longing which was something like a call of what lay through the wall. Unfortunately, he lacked an objective.',
+  'He looked for inspiration in the newspaper, particularly in the politics activities, but finally in realising that they offered no outlets',
+  'he made do with the most promising of the ‘brief’ news items. His eyes shone with warmth and gratitude.',
+  'For a man who walk through walls, there can be no dazzling career if he hasn’t at least seen the inside of a prison.',
+  'The following morning, Dutilleul was annoyed to wake up with a nasty headache. It didn’t bother him badly and he wasn’t going to let such a',
+  'minor thing keep him form his next rendezvous. Still, when he happened to find a few pills scattered at the back of a drawer, he gulped down',
+  'one that morning and one in the afternoon. By evening, his headache was bearable and in his elation, he managed to forget it completely.',
+  'When he was leaving, while walking through the partitions and walls of the house, Dutilleul had an unfamiliar feeling.',
+  ''
+]
+
+story_6_parts = [
+  'The orchestra played for them.',
+  'They ran into the night.',
+  'Daphnis: You make good food.',
+  'Chloe: You’re good in bed.',
+  '',
+  '',
+  '',
+  '',
+  '',
+  ''
+]
+
+story_7_parts = [
+  'He looked for inspiration in the newspaper, particularly in the politics activities, but finally in realising that they offered no outlets',
+  'he made do with the most promising of the ‘brief’ news items. His eyes shone with warmth and gratitude.',
+  'For a man who walk through walls, there can be no dazzling career if he hasn’t at least seen the inside of a prison.',
+  'The following morning, Dutilleul was annoyed to wake up with a nasty headache. It didn’t bother him badly and he wasn’t going to let such a',
+  'minor thing keep him form his next rendezvous. Still, when he happened to find a few pills scattered at the back of a drawer, he gulped down',
+  'one that morning and one in the afternoon. By evening, his headache was bearable and in his elation, he managed to forget it completely.',
+  '',
+  '',
+  '',
+  ''
+]
+
+story_8_parts = [
+  'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?',
+  'At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.',
+  'Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit.',
+  'Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae.',
+  '',
+  '',
+  '',
+  '',
+  '',
+  ''
+]
+
+
+story_9_parts = [
+  'Bresaola ribeye landjaeger ham hock pastrami pork chop tongue pork loin meatball jowl burgdoggen.',
+  'Doner corned beef pig drumstick short ribs tenderloin turducken, spare ribs shankle prosciutto biltong leberkas.',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  ''
+]
 
 users = []
 stories = []
-# parts = []
 
 User.delete_all
 Story.delete_all
-# Part.delete_all
+Part.delete_all
 
-10.times do |i|
+11.times do |i|
   user = User.new
   user.name = usernames[i]
   user.email = emails[i]
   user.password = passwords[i]
   user.description = descriptions[i]
-  user.photo_url = 'https://placeholdit.imgix.net/~text?txtsize=14&txt=150%C3%97150&w=150&h=150'
+  user.photo_url = 'http://placehold.it/150x150'
   user.save
   users << user
 end
 
-10.times do |i|
+6.times do |i|
   story = Story.new
   story.title = story_titles[i]
   story.save
@@ -115,10 +224,92 @@ end
 end
 
 # Generating parts will run into story and timestamp issues
+# Run these commands one at a time
 
-# 10.times do |i|
-#   part = Part.new
-#   part.user_id =
-#   part.story_id =
-#   parts << part
-# end
+# Story 1
+story_1_parts.length.times do |i|
+  part = Part.new
+  part.user_id = i + 1
+  part.story_id = 1
+  part.body = story_1_parts[i]
+  part.save
+end
+# need to delete part 11
+
+# Story 2
+1.times do |i|
+  part = Part.new
+  part.user_id = i + 4
+  part.story_id = 2
+  part.body = story_2_parts[i]
+  part.save
+end
+
+# Story 3
+11.times do |i|
+  part = Part.new
+  part.user_id = i + 1
+  part.story_id = 3
+  part.body = story_3_parts[i]
+  part.save
+end
+# need to delete part 11
+
+# Story 4
+3.times do |i|
+  part = Part.new
+  part.user_id = i + 2
+  part.story_id = 4
+  part.body = story_4_parts[i]
+  part.save
+end
+
+# Story 5
+11.times do |i|
+  part = Part.new
+  part.user_id = i + 1
+  part.story_id = 5
+  part.body = story_5_parts[i]
+  part.save
+end
+# need to delete part 11
+
+# Story 6
+4.times do |i|
+  part = Part.new
+  part.user_id = i + 2
+  part.story_id = 6
+  part.body = story_6_parts[i]
+  part.save
+end
+
+# Story 7
+story_7_parts.length.times do |i|
+  part = Part.new
+  part.user_id = i + 1
+  part.story_id = 7
+  part.body = story_6_parts[i]
+  part.save
+end
+
+# Story 8
+story_8_parts.length.times do |i|
+  part = Part.new
+  part.user_id = i + 1
+  part.story_id = 8
+  part.body = story_6_parts[i]
+  part.save
+end
+
+# Story 9
+story_9_parts.length.times do |i|
+  part = Part.new
+  part.user_id = i + 1
+  part.story_id = 9
+  part.body = story_6_parts[i]
+  part.save
+end
+
+Part.where(story_id: 1).last.destroy
+Part.where(story_id: 3).last.destroy
+Part.where(story_id: 5).last.destroy
