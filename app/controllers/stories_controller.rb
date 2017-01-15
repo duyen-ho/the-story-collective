8623984ec0c30954d538eca751de2450ae61a16e
Duyen Ho <duyenho@gmail.com>
2017-01-15 19:50:53 +1100

    rewrite stories controller and views
    separation of concerns: add helper method, remove active record queries
    from views, refactor code in index and show pages

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,12 +1,26 @@
 class StoriesController < ApplicationController
 
+  helper_method :get_username
+
+  def get_username part
+    return User.find(part.user_id).name
+  end
+
   def index
-    @stories = Story.all
+    @completed_stories = Story.where(is_completed: true)
+    @open_stories_parts = []
+
+    open_stories = Story.where(is_completed: false)
+    open_stories.each do |story|
+      @open_stories_parts << Part.find_by(story_id: story.id)
+    end
+
   end
 
   def show
     @story = Story.find_by(id: params[:id])
     @parts = Part.where(story_id: params[:id]).order(:created_at)
+
     if @story
       render :show
     else
